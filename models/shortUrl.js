13a2b8399c637d8115ec991229b376cbc53c4ebd
Amisha <amisha1324s@gmail.com>
2024-03-01 22:44:42 +0530

    suggestions

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const base="lost";
+const shortId = require('shortid');
+
 const shortUrlSchema = new mongoose.Schema({
   full: {
     type: String,
@@ -8,46 +9,18 @@ const shortUrlSchema = new mongoose.Schema({
   suggest: {
     type: String,
   },
+  short: {
+    type: String,
+    required: true,
+    default: function () {
+      const base = shortId.generate();
+      return this.suggest ? `${base}-${this.suggest}` : base;
+    }
+  },
   clicks: {
     type: Number,
     required: true,
     default: 0
-  }
-});
-
-// Create a separate counter schema
-const CounterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
-});
-
-const Counter = mongoose.model('Counter', CounterSchema);
-
-shortUrlSchema.pre('save', async function (next) {
-  const doc = this;
-  if (!doc.isNew) {
-    // If the document is not new, do nothing
-    return next();
-  }
-
-  try {
-    // Use findOneAndUpdate to increment the counter and get the updated value
-    const counter = await Counter.findOneAndUpdate(
-      { _id: 'shortUrlCounter' },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
-    );
-
-    // Set the short URL based on the counter and suggest value
-    if (doc.suggest) {
-      doc.short = `${base}-${counter.seq}-${doc.suggest}`;
-    } else {
-      doc.short = `${base}-${counter.seq}`;
-    }
-
-    next();
-  } catch (error) {
-    return next(error);
   }
 });
 
